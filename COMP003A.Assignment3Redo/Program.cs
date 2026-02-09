0f2b74306d4cb3df38b448d00b52d1140e52c27e
Isaiah-cto <isaiahcastillo285@gmail.com>
2026-02-09 01:04:42 -0800

    commit all strings & bools

--- COMP003A.Assignment3Redo/Program.cs
+++ COMP003A.Assignment3Redo/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Current Year: ");
+            DateTime Now = DateTime.Now;
+            int currentYear = Now.Year;
+            Console.Write("Enter your birth year: ");
+            string name = Console.ReadLine();
+            int age = DateTime.Now.Year;
+            string ReadinessCategory = age >= 18 ? "Adult" : "Child";
+            Console.WriteLine($"Calculated Age: 21");
+            Console.WriteLine($"Readiness Category: Adult");
+            Console.Write("Do you have a valid ID? (yes/no): ");
+            bool hasvalidID = Console.ReadLine() == ("yes");
+            Console.Write("Have you completed orientation? (yes/no): ");
+            bool completedorientation = Console.ReadLine() == ("yes");
+            Console.Write("Readiness Status: ");
+            if (hasvalidID && completedorientation)
+            {
+                Console.WriteLine("Ready");
+            }
+            else if (hasvalidID || completedorientation)
+            {
+                Console.WriteLine("Conditionally Ready");
+            }
         }
     }
 }
